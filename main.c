2ba37b8a26d06dff0d884a63b12fe6882f9877ad
verandert <verandert@163.com>
2020-05-07 03:03:19 +0800

    update main

--- main.c
+++ main.c
@@ -1,30 +1,30 @@
 /*
  * @Author: verandert
  * @Date: 2020-05-03 13:53:58
- * @LastEditTime: 2020-05-06 13:43:13
+ * @LastEditTime: 2020-05-07 01:28:44
  * @Description: main function
  */
 #include "./include/graph.h"
 
 int main(){
-    VerType ver[] = {1,2,3,4,5,6}, arc[][2] = {{1,2}, {1,3}, {2, 4}, {3, 2}, {3, 4}, {4, 1}, {5, 6}};
-    enum bool visited[6];
-    for (int i = 0; i < 6; i++)
+    VerType ver[] = {1,2,3,4}, arc[][3] = {{1, 2, 6}, {2, 1, 6}, {2, 3, 2}, {3, 2, 2}, {3, 4, 4}, {4, 3, 4}};
+    enum bool visited[4];
+    for (int i = 0; i < 4; i++)
     {
         visited[i] = false;
     }
     //boolean visited[4];
     OrtGraph G;
     OrtGraph Gr;
-    G.arcnum = 7; G.vernum = 6;
-    Gr.arcnum = 7; Gr.vernum = 6;
+    G.arcnum = 6; G.vernum = 4;
+    Gr.arcnum = 6; Gr.vernum = 4;
     CreateOG(&G, ver, arc);
-    CreateReverseOG(&Gr, ver, arc);
-    FSCCbyKosaraju(&G, &Gr, visited);
+    MSTByPrim(&G);
+    //CreateReverseOG(&Gr, ver, arc);
+    //FSCCbyKosaraju(&G, &Gr, visited);
     //DFSForest(&G, visited, &p);
     //DFSTraverse(&G, visited);
     //BFSTraverse(&G, visited);
     //DFSTraverseFSCC(&G, visited);
-    //ReverseDFSTraverse(&G, visited);
     return 0;
 }
