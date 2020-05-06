f86a53f3a800343d3c2bccb53f880aa696eb509b
verandert <verandert@163.com>
2020-05-06 13:56:44 +0800

    update main function

--- main.c
+++ main.c
@@ -1,22 +1,30 @@
 /*
  * @Author: verandert
  * @Date: 2020-05-03 13:53:58
- * @LastEditTime: 2020-05-04 17:34:56
+ * @LastEditTime: 2020-05-06 13:43:13
  * @Description: main function
  */
 #include "./include/graph.h"
 
 int main(){
-    VerType ver[] = {1,2,3,4,5,6}, arc[][2] = {{1,2}, {1,3}, {3,2}, {3, 4}, {4, 1}, {5, 6}};
+    VerType ver[] = {1,2,3,4,5,6}, arc[][2] = {{1,2}, {1,3}, {2, 4}, {3, 2}, {3, 4}, {4, 1}, {5, 6}};
     enum bool visited[6];
+    for (int i = 0; i < 6; i++)
+    {
+        visited[i] = false;
+    }
     //boolean visited[4];
     OrtGraph G;
-    CBNode T, *p;
-    p = &T;
-    G.arcnum = 6; G.vernum = 6;
+    OrtGraph Gr;
+    G.arcnum = 7; G.vernum = 6;
+    Gr.arcnum = 7; Gr.vernum = 6;
     CreateOG(&G, ver, arc);
-    DFSForest(&G, visited, &p);
+    CreateReverseOG(&Gr, ver, arc);
+    FSCCbyKosaraju(&G, &Gr, visited);
+    //DFSForest(&G, visited, &p);
     //DFSTraverse(&G, visited);
     //BFSTraverse(&G, visited);
+    //DFSTraverseFSCC(&G, visited);
+    //ReverseDFSTraverse(&G, visited);
     return 0;
 }
